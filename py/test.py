d6f99b33ee3845d512c3d8311c5ae356df932fc4
wangyibo <1156873578@qq.com>
2022-12-09 11:57:04 +0800

    add test

--- py/test.py
+++ py/test.py
@@ -1,2 +1,3 @@
-#!/usr/bin/env python 
+#!/usr/bin/python3 
 print("hello world!")
+print([a for m in range(100,7) for a in range(1000) if (a%m == m) ])
---
+++ py1/test..py
@@ -0,0 +1,3 @@
+import json 
+wang = list(("wagn",333,33333,3,True,False,False))
+# print(wang.sort())
