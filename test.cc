25eba9e629353423000e50bdbcb5aef7d3d4fac9
wangyibo <1156873578@qq.com>
2023-01-17 17:59:11 +0800

    1

--- test.cc
+++ test.cc
@@ -4,32 +4,40 @@
 #include <array>
 #include <queue>
 #include <iterator>
-
-int main(int argc, char **argv)
+int * function (int arg,int a =10)
 {
-    std::vector<int> v;
-    std::list<int> List{1, 2, 3, 4, 5, 6};
-    List.push_back(1);
-    for (auto &a : List)
-    {
-        std::cout << a << " " << std::endl;
-    }
+    int * wang = new int ;
+    *wang = arg+a;
+    return wang;
+}
+int main(int argc, char **argv)
+{   
+    int * wang = function(10,100);
+    std::cout <<*wang <<std::endl;
+    delete wang;
+    // std::vector<int> v;
+    // std::list<int> List{1, 2, 3, 4, 5, 6};
+    // List.push_back(1);
+    // for (auto &a : List)
+    // {
+    //     std::cout << a << " " << std::endl;
+    // }
 
-    auto it = List.begin();
-	while(1)
-	{
-		if(*it != 3)
-			break;
-	}
-    List.insert(it, 10);
-    for (auto &a : List)
-    {
-        std::cout << a << " " << std::endl;
-    }
+    // auto it = List.begin();
+	// while(1)
+	// {
+	// 	if(*it != 3)
+	// 		break;
+	// }
+    // List.insert(it, 10);
+    // for (auto &a : List)
+    // {
+    //     std::cout << a << " " << std::endl;
+    // }
 
-    List.sort();
-    for (auto &a : List)
-    {
-        std::cout << a << " " << std::endl;
-    }
+    // List.sort();
+    // for (auto &a : List)
+    // {
+    //     std::cout << a << " " << std::endl;
+    // }
 }
